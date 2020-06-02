88b1ba1adf41e565550595c3c5b75ecc9c4b87ea
Omer Abdulaziz <omerabdi@protonmail.com>
2020-06-02 21:42:43 +0300

    Initail

---
+++ controllers/userCont.js
@@ -0,0 +1,13 @@
+// export function signup(req, res, next) {
+//     res.status('200').json({
+//         status: 'success',
+//     });
+// }
+
+exports.signup = (req, res, next) => {
+	
+
+	res.status('200').json({
+		status: 'success'
+	});
+};
--- start.js
+++ start.js
@@ -3,6 +3,11 @@ const userRouter = require('./routes/userRoutes');
 
 app = express();
 
+// body parser, put data from body to req.body
+app.use(express.json({ limit: '10kb' }));
+
+
+
 // routes
 app.use('/api/users', userRouter);
 
